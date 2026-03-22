8111d584a12811144e8b46bbcff1f4134ba77f5f
hemraj11 <hemrajojha1122334455@gmail.com>
2026-03-22 20:36:19 +0545

    Add color constants

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const primary = Color(0xFFF85604);
+  static const textDark = Color(0xFF101828);
+  static const textLight = Color(0xFF6A7282);
+  static const backgroundColor = Color(0xFFFAFAFA);
+  static const boxColor = Color(0xFFFFFFFF);
+}
