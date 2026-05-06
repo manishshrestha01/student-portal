77ce806a7fa2ff671d56948323b7fed84080fe00
manishshrestha01 <shresthamanish8080@gmail.com>
2026-05-07 00:29:00 +0545

    Add meet link functionality

--- lib/view/link_view.dart
+++ lib/view/link_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LinkView extends StatefulWidget {
   const LinkView({super.key});
@@ -32,89 +33,96 @@ class _LinkViewState extends State<LinkView> {
         return const SizedBox.shrink();
       }
 
-      final item = linksList.first;
-      return Container(
-        width: double.infinity,
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: const Color(0xFFFFFFFF)),
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withAlpha(64),
-              blurRadius: 4,
-              offset: const Offset(0, 4),
+      return Column(
+        children: linksList.map((item) {
+          return Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(bottom: 16),
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: const Color(0xFFFFFFFF)),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withAlpha(64),
+                  blurRadius: 4,
+                  offset: const Offset(0, 4),
+                ),
+              ],
             ),
-          ],
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              width: 4,
-              height: 120,
-              decoration: BoxDecoration(color: const Color(0xFF00c951)),
-            ),
-            Gap(16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "${item.courseName}",
-                    style: GoogleFonts.inter(
-                      textStyle: TextStyle(
-                        color: AppColors.textDark,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    "${item.mentorName}",
-                    style: GoogleFonts.inter(
-                      textStyle: TextStyle(
-                        color: const Color(0xFF4a5565),
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    "Class Time: ${item.classTime}",
-                    style: GoogleFonts.inter(
-                      textStyle: TextStyle(
-                        color: AppColors.textLight,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF00a63e),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    child: Text(
-                      "Join Live Class",
-                      style: GoogleFonts.inter(
-                        textStyle: TextStyle(
-                          color: AppColors.boxColor,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w700,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 4,
+                  height: 120,
+                  decoration: BoxDecoration(color: const Color(0xFF00c951)),
+                ),
+                Gap(16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${item.courseName}",
+                        style: GoogleFonts.inter(
+                          textStyle: TextStyle(
+                            color: AppColors.textDark,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
                       ),
-                    ),
+                      Text(
+                        "${item.mentorName}",
+                        style: GoogleFonts.inter(
+                          textStyle: TextStyle(
+                            color: const Color(0xFF4a5565),
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Class Time: ${item.classTime}",
+                        style: GoogleFonts.inter(
+                          textStyle: TextStyle(
+                            color: AppColors.textLight,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () async{
+                          launchUrl(Uri.parse(item.meetLink ?? ''));
+                          
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF00a63e),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Text(
+                          "Join Live Class",
+                          style: GoogleFonts.inter(
+                            textStyle: TextStyle(
+                              color: AppColors.boxColor,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        }).toList(),
       );
     });
   }
