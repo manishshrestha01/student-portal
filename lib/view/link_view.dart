8afb6c02fb2e9be337082715a6f947cec6812af9
manishshrestha01 <shresthamanish8080@gmail.com>
2026-05-06 22:45:16 +0545

    added green line

--- lib/view/link_view.dart
+++ lib/view/link_view.dart
@@ -1,6 +1,7 @@
 import 'package:codeit_app/controller/link_controller.dart';
 import 'package:codeit_app/core/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -47,56 +48,69 @@ class _LinkViewState extends State<LinkView> {
             ),
           ],
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text(
-              "${item.courseName}",
-              style: GoogleFonts.inter(
-                textStyle: TextStyle(
-                  color: AppColors.textDark,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
+            Container(
+              width: 4,
+              height: 120,
+              decoration: BoxDecoration(color: const Color(0xFF00c951)),
             ),
-            Text(
-              "${item.mentorName}",
-              style: GoogleFonts.inter(
-                textStyle: TextStyle(
-                  color: const Color(0xFF4a5565),
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            Text(
-              "Class Time: ${item.classTime}",
-              style: GoogleFonts.inter(
-                textStyle: TextStyle(
-                  color: AppColors.textLight,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF00a63e),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: Text(
-                "Join Live Class",
-                style: GoogleFonts.inter(
-                  textStyle: TextStyle(
-                    color: AppColors.boxColor,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w700,
+            Gap(16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "${item.courseName}",
+                    style: GoogleFonts.inter(
+                      textStyle: TextStyle(
+                        color: AppColors.textDark,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
-                ),
+                  Text(
+                    "${item.mentorName}",
+                    style: GoogleFonts.inter(
+                      textStyle: TextStyle(
+                        color: const Color(0xFF4a5565),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Class Time: ${item.classTime}",
+                    style: GoogleFonts.inter(
+                      textStyle: TextStyle(
+                        color: AppColors.textLight,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF00a63e),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      "Join Live Class",
+                      style: GoogleFonts.inter(
+                        textStyle: TextStyle(
+                          color: AppColors.boxColor,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
